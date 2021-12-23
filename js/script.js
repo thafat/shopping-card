e295a977e6529dbede403e8d1c6cafe1eea3acae
amira <amira.amrouche.kadid@gmail.com>
2021-12-23 09:50:58 +0100

    V4

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//1.Select ligne1,prix,increase,decrease,delete,quant,total,image.
+//2.write function calculatePrice then call it
+function calculateTotalPrice(){
+    //calculate total price using for loop
+    //display total price
+}
+calculateTotalPrice()
+
+//3.Add event to increament buttons==use for
+//4.Add event to decreament buttons==use for
+//5.Add event to delete button==use for
+//6.Add event to like icons(if icon is red make it black else make it red)
+
+let ligne1=document.querySelector(".ligne1")
+console.log ("ligne1: ",ligne1)
+
+let prix=document.querySelector(".prix")
+console.log ("prix: ",prix)
+
+let increase=document.querySelector (".increase")
+console.log ("increase: ",increase)
+
+let decrease=document.querySelector (".decrease")
+console.log ("decrease: ",decrease)
